4fb433767f3e7fe8a31f9eb7b318f07420884d41
simonedimeglio <simonedimeglio@icloud.com>
2021-07-08 15:22:19 +0200

    1st code refactoring

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 Vue.config.devtools = true;
-
-
-// Chiamare l'api per poter generare un numero randomico (che ci restuisce il backend)
-// e mostrare questo numero a schermo.
-
-// => dobbiamo popolare un data (che all'inizio è 0), e mostrarlo a schermo {{ mails }}
-
 new Vue(
     {
         el: '#app',
         data: {
-            mails: []
+            mails: [] // array vuoto di mail
         },
         created() {
+            // richiamo il methods
             getMails();
         },
         methods: {
-            getMails: function () {
-                for (let i = 0; i < 10; i++) {
-                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            getMails: function () { // creo la funzione per generare le mail
+                
+                for (let i = 0; i < 10; i++) { // fino a che l'array non ha lunghezza pari a 10
+
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail') // get dall'API
+
                         .then((response) => {  // appena il server ci risponde, si attiva questa funzione
-                            this.mails.push(response.data.response);
+
+                            this.mails.push(response.data.response); // pusho nell'array il dato generato dall'API
                         });
                 }
             }
